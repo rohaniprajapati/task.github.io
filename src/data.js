ebe62e1a6453ff5533f6d42fdef36cf87f639cd5
rohani prajapati <rohaniprajapati256@gmail.com>
2022-07-01 16:07:37 +0530

    hii
    happy

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const list = [
+  {
+    id: 1,
+    title: "Hamburger",
+    price:200,
+    img: "images/r1.jpg",
+    amount: 1,
+   
+  },
+  {
+    id: 2,
+    title: "Fries",
+    author:"total:1",
+    price:100,
+    img: "images/r2.jpg",
+    amount: 1,
+    
+  },
+  {
+    id: 3,
+    title: "Coke",
+    author: "total:2",
+    img: "images/r3.jpg",
+    price:50 ,
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: "Pepsi",
+    author: "total:2",
+    price: 50,
+    img: "images/r4.jpg",
+    amount: 1,
+  },
+  
+   
+
+];
+
+export default list;
